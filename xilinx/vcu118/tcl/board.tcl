b294a5bc423b9d2592e8d5f80e84a518aba909b3
John Wright <johnwright@eecs.berkeley.edu>
2019-07-23 14:28:36 -0700

    Revert "Use engineering silicon version of this board for our stuff"
    This reverts commit 5f1acbdafe5a516a8f7b42d9489287466e35e796.

--- xilinx/vcu118/tcl/board.tcl
+++ xilinx/vcu118/tcl/board.tcl
@@ -1,7 +1,6 @@
 # See LICENSE for license details.
 set name {vcu118}
-#set part_fpga {xcvu9p-flga2104-2L-e}
-set part_fpga {xcvu9p-flga2104-2LV-e-es1}
+set part_fpga {xcvu9p-flga2104-2L-e}
 # Board: xilinx.com:vcu118:part0:2.0
 # However, if we set this we cannot control PCIe locations
 set part_board {}
